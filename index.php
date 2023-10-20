8f15151edd0533c23899e7f95f031079808156bb
Alex Kirk <akirk@users.noreply.github.com>
2023-10-20 11:12:11 +0200

    Secret via URL

--- index.php
+++ index.php
@@ -2,7 +2,12 @@
 $index = file_get_contents( __DIR__ . '/index.html' );
 $secret = getenv( 'REVEALJS_SECRET' );
 if ( ! $secret ) {
-	echo $index;
-	exit;
+	if  ( isset( $_GET['secret'] ) ) {
+		$secret = $_GET['secret'];
+	}
+	if ( ! $secret ) {
+		echo $index;
+		exit;
+	}
 }
 echo str_replace( 'https://reveal-multiplex.glitch.me/client.js', 'https://reveal-multiplex.glitch.me/master.js', str_replace( 'secret: null', 'secret: "' .  $secret . '"', $index ) );
