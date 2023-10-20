7b3f88aa232cd8d340c25abcbbbd3e62537b5db0
Alex Kirk <akirk@users.noreply.github.com>
2023-10-20 10:41:58 +0200

    Don't serve master if no secret is present

--- index.php
+++ index.php
@@ -1,2 +1,8 @@
 <?php
-echo str_replace( 'https://reveal-multiplex.glitch.me/client.js', 'https://reveal-multiplex.glitch.me/master.js', str_replace( 'secret: null', 'secret: "' .  getenv( 'SECRET' ) . '"', file_get_contents( __DIR__ . '/index.html' ) ) );
+$index = file_get_contents( __DIR__ . '/index.html' );
+$secret = getenv( 'REVEALJS_SECRET' );
+if ( ! $secret ) {
+	echo $index;
+	exit;
+}
+echo str_replace( 'https://reveal-multiplex.glitch.me/client.js', 'https://reveal-multiplex.glitch.me/master.js', str_replace( 'secret: null', 'secret: "' .  $secret . '"', $index ) );
